7e8ec48d80f4f315dfe8b940c50d6ca04a8b5b58
yuki <nkc.yuki.koyama@gmail.com>
2017-09-19 10:59:49 +0900

    no message

---
+++ Assets/koyama/Lift controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Liftcontroller : MonoBehaviour
+{
+    Animator anima;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //anima = GetComponent<Animator>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //オブジェクトが触れている間
+    void OnCollisionStay2D(Collision collision)
+    {
+
+    }
+}
---
+++ Assets/koyama/SetTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTimer : MonoBehaviour
+{
+    public int minite = 0;
+    public float second = 0;
+    //private int oldSecond = 0;
+    public Text textField;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        if (Time.timeScale > 0)
+        {
+            second += Time.deltaTime;
+            
+            if (second > 60.0f)
+            {
+                minite++;
+                second = 0;
+            }
+            //second = Mathf.Floor(0.5f);
+            textField.text = "タイム：" + minite + "分" + second.ToString("f0") + "秒";      
+        }
+	}
+}
