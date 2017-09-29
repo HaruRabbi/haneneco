e308a2046871320316cd426fd2ab34d1bb4b6564
nakashima <nkc.kazuki.nakashima@gmail.com>
2017-09-29 14:19:16 +0900

    細かい調整やる↑↑

--- Assets/Script/GateSwitch.cs
+++ Assets/Script/GateSwitch.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class GateSwitch : MonoBehaviour
 {
     public Gateup gateup;
+    public Sprite[] swichSprite; //0にデフォルト画像、１に変えたい画像を入れてね♡
+    SpriteRenderer swichSp;
+
 
     public void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Player")
         {
             gateup.Up();
+            swichSp.sprite = swichSprite[1];
         }
     }
 
@@ -24,12 +28,14 @@ public class GateSwitch : MonoBehaviour
     public void InvokeDown()
     {
         gateup.Down();
+        swichSp.sprite = swichSprite[0];
     }
 
     // Use this for initialization
     void Start()
     {
-
+        swichSp = GetComponent<SpriteRenderer>();
+        swichSp.sprite = swichSprite[0];
     }
 
     // Update is called once per frame
