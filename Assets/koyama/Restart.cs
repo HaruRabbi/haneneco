af5bf82969eade308c826f93103e7b5aa2481eeb
yuki <nkc.yuki.koyama@gmail.com>
2017-09-29 11:46:44 +0900

    タイトルのBGMを追加(ついでにゲームオーバー時とクリア時の効果音も追加で実装)

--- Assets/koyama/Restart.cs
+++ Assets/koyama/Restart.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Restart : MonoBehaviour {
+public class Restart : MonoBehaviour
+{
+    private AudioSource audioSource;// AudioSorceコンポーネント格納用
+    public AudioClip sound;// 効果音の格納用。インスペクタで。
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start ()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.loop = false;
+        audioSource.PlayOneShot(sound);
+    }
 	
 	// Update is called once per frame
 	void Update ()
--- Assets/koyama/SceneSelect.cs
+++ Assets/koyama/SceneSelect.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneSelect : MonoBehaviour
 {
+    private AudioSource audioSource;// AudioSorceコンポーネント格納用
+    public AudioClip sound;// 効果音の格納用。インスペクタで。
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.loop = false;
+        audioSource.PlayOneShot(sound);
+    }
+
     // Update is called once per frame
     void Update()
     {
