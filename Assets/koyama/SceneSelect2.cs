a73764092031d9c3907be28b8848f3f16af72261
nakashima <nkc.kazuki.nakashima@gmail.com>
2017-10-04 11:30:12 +0900

    第2ステージから第3ステージへとのシーン移動ｺﾋﾟって作った

---
+++ Assets/koyama/SceneSelect2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSelect2 : MonoBehaviour
+{
+    private AudioSource audioSource;// AudioSorceコンポーネント格納用
+    public AudioClip sound;// 効果音の格納用。インスペクタで。
+                           // Use this for initialization
+    void Start ()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.loop = false;
+        audioSource.PlayOneShot(sound);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            SceneManager.LoadScene("Fade-in");
+        }
+        if (Input.GetKey(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Main3");
+        }
+    }
+}
